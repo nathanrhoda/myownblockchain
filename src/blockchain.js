0305eb12fa973cd825880d32729eb4274628137d
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-07-11 21:28:29 +0200

    updated filter to find and removed index

--- src/blockchain.js
+++ src/blockchain.js
@@ -169,7 +169,7 @@ class Blockchain {
     getBlockByHeight(height) {
         let self = this;
         return new Promise((resolve, reject) => {
-            let block = self.chain.filter(p => p.height === height)[0];
+            let block = self.chain.fimd(p => p.height === height);
             if(block){
                 resolve(block);
             } else {
